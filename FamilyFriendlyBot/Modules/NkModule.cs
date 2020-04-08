3eea79926cb7a99603d6df1498341dbdae381b94
Piotr Madura <piotrek.marchewka123@gmail.com>
2020-04-08 22:42:31 +0200

    Szukanie tekstem

--- FamilyFriendlyBot/Modules/NkModule.cs
+++ FamilyFriendlyBot/Modules/NkModule.cs
@@ -69,6 +69,64 @@ namespace FamilyFriendlyBot.Modules
 
             await ReplyAsync("", false, builder.Build());
         }
+
+        [Command("video")]
+        [Summary("Daje losowy filmik pasujący do wyszukiwania ze strony Pornhub")]
+        [Alias("filmik")]
+        public async Task VideoAsync(
+            [Summary("Rzeczy do wyszukania")]string query,
+            [Summary("Liczba stron do przeszukania")]int pages = 1)
+        {
+            if (pages > 10 || pages < 1)
+                await ReplyAsync($"Argument **pages** nie może być równy {pages}, musi się zawierać między 1 a 10 włącznie.");
+
+            var browser = new ScrapingBrowser();
+
+            query = query.Replace(' ', '+');
+
+            List<PornVideo> videos = new List<PornVideo>();
+
+            for (int i = 1; i <= pages; i++)
+            {
+                Uri searchUri = new Uri(website, $"/video/search?search={query}&page={i}");
+
+                WebPage page = browser.NavigateToPage(searchUri);
+
+                var nodes = page.Html.CssSelect("a.linkVideoThumb");
+
+                foreach (var video in nodes)
+                {
+                    string title = WebUtility.HtmlDecode(video.GetAttributeValue("title"));
+                    Uri fullVideoUrl = new Uri(website, video.GetAttributeValue("href"));
+                    var imgNode = video.ChildNodes.ToArray()[1];
+                    Uri imageUri = new Uri(imgNode.GetAttributeValue("data-thumb_url"));
+                    videos.Add(new PornVideo
+                    {
+                        Title = title,
+                        VideoUri = fullVideoUrl,
+                        ThumbnailUri = imageUri
+                    });
+                }
+            }
+
+            int itemNum = _rand.Next(0, videos.Count - 1);
+
+            var builder = new EmbedBuilder()
+            {
+                Color = new Color(0xFFA31A),
+                Title = "Filmik dla ciebie",
+                Description = videos[itemNum].Title
+            };
+            builder.WithImageUrl(videos[itemNum].ThumbnailUri.ToString());
+            builder.AddField(new EmbedFieldBuilder()
+            {
+                Name = $"Link: {videos[itemNum].VideoUri}",
+                Value = $"Obejrzyj go [TUTAJ]({videos[itemNum].VideoUri})"
+            });
+            builder.WithCurrentTimestamp();
+
+            await ReplyAsync("", false, builder.Build());
+        }
     }
 
     internal class PornVideo
